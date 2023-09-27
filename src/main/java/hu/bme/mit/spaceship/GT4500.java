148788222f46b8bd9ceea1833a392c2c35cd5cef
Meszaros Barna <barna.meszaros02@gmail.com>
2023-09-27 15:19:28 +0200

    FixAllMissiles2

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,6 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
         if(!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty() )
           //try to fire the primary and the secondary
           firingSuccess = (primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1));
